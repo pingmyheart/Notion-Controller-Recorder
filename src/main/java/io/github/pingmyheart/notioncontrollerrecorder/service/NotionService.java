4b66f2fe17e40463bcf6d25061f5c73f7de9b507
GitlabCiCd <antonio.russi.consulting@gmail.com>
2025-06-03 18:03:52 +0200

    [REMOVE] - fixed todo

--- src/main/java/io/github/pingmyheart/notioncontrollerrecorder/service/NotionService.java
+++ src/main/java/io/github/pingmyheart/notioncontrollerrecorder/service/NotionService.java
@@ -3,7 +3,6 @@ package io.github.pingmyheart.notioncontrollerrecorder.service;
 import io.github.pingmyheart.notioncontrollerrecorder.dto.internal.ReportDTO;
 
 public interface NotionService {
-    //TODO handle next cursor for pagination tio retrieve all pages
     String getOrCreateProjectId(String notionPageId,
                                 String projectName);
 
